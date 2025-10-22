7dbe7aa150bfb9975de0c3a7ce416e818a23c943
nicolasandradedesarrollosit <nicolasandradedesarrollos@gmail.com>
2025-10-22 10:58:22 -0300

    🐛 fix: error html meta

--- src/components/Home/FirstSection.tsx
+++ src/components/Home/FirstSection.tsx
@@ -9,7 +9,7 @@ import {
 
 function FirstSection() {
     return (
-        <div className="flex flex-col items-center min-h-screen py-16 bg-white gap-8 sm:gap-10 md:gap-12 py-8 sm:py-12 md:py-16">
+        <div className="flex flex-col items-center min-h-screen bg-white gap-8 sm:gap-10 md:gap-12 py-8 sm:py-12 md:py-16">
             <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-primary px-4 text-center">Como trabajamos?</p>
             <p className="text-sm sm:text-base md:text-lg text-black w-full sm:w-4/5 md:w-3/4 lg:w-2/3 text-center opacity-50 px-4">En Andart nosotros nos dedicamos a solucionar problemas, brindando soluciones informaticas para todo tipo de empresas</p>
             <div className="flex flex-col sm:flex-row sm:flex-wrap w-full sm:w-4/5 md:w-3/4 lg:w-2/3 items-center justify-center gap-6 sm:gap-8 px-4">
